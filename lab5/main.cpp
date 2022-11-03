cba6767212db97197737eafef0b2eb2fcf1b0212
Дмитрий <50549999+MinusD@users.noreply.github.com>
2022-11-03 10:00:51 +0300

    Added Practice 5 (pre-end)

--- lab5/main.cpp
+++ lab5/main.cpp
@@ -21,90 +21,178 @@ struct Node {
      * Структура, описывающая вершину графа
      */
     string name;
-    vector<Node *> neighbours; // Список смежных вершин
-    vector<int> distances; // Список расстояний (весов) ребер
 };
 
 class Graph {
     /*
      * Класс, описывающий граф
      */
-    vector<Node *> nodes; // Список вершин графа
+
     int size; // Количество вершин графа
+    vector<vector<int>> matrix; // Матрица смежности
+    vector<Node *> nodes; // Список вершин графа
 
-    public:
-        Graph() {
-            /*
-             * Конструктор класса
-             */
-            size = 0;
+public:
+    Graph() {
+        /*
+         * Конструктор класса
+         */
+        size = 0;
+    }
+
+    void addNode(string name) {
+        /*
+         * Добавление вершины в граф
+         */
+        Node *node = new Node;
+        node->name = name;
+        nodes.push_back(node);
+        size++;
+        matrix.resize(size);
+        for (int i = 0; i < size; i++) {
+            matrix[i].resize(size);
         }
+    }
 
-        void addNode(string name) {
-            /*
-             * Добавление вершины в граф
-             */
-            Node *node = new Node;
-            node->name = name;
-            nodes.push_back(node);
-            size++;
+    void addEdge(string name1, string name2, int distance) {
+        /*
+         * Добавление ребра в граф
+         */
+        int index1 = -1;
+        int index2 = -1;
+        for (int i = 0; i < size; i++) {
+            if (nodes[i]->name == name1) {
+                index1 = i;
+            }
+            if (nodes[i]->name == name2) {
+                index2 = i;
+            }
         }
-
-        void addEdge(string name1, string name2, int distance) {
-            /*
-             * Добавление ребра в граф
-             */
-            Node *node1 = getNode(name1);
-            Node *node2 = getNode(name2);
-            node1->neighbours.push_back(node2);
-            node1->distances.push_back(distance);
-            node2->neighbours.push_back(node1);
-            node2->distances.push_back(distance);
+        if (index1 == -1 || index2 == -1) {
+            cout << "Error: node not found" << endl;
+            return;
         }
+        matrix[index1][index2] = distance;
+        matrix[index2][index1] = distance;
+    }
 
-        Node *getNode(string name) {
-            /*
-             * Получение вершины по имени
-             */
-            for (int i = 0; i < size; i++) {
-                if (nodes[i]->name == name) {
-                    return nodes[i];
+    Node *getNode(string name) {
+        /*
+         * Получение вершины по имени
+         */
+        for (int i = 0; i < size; i++) {
+            if (nodes[i]->name == name) {
+                return nodes[i];
+            }
+        }
+        return nullptr;
+    }
+
+    int getIndex(string name) {
+        /*
+         * Получение индекса вершины по имени
+         */
+        for (int i = 0; i < size; i++) {
+            if (nodes[i]->name == name) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    void print() {
+        /*
+         * Вывод графа на экран
+         */
+
+        vector<string> printedTo;
+        vector<string> printedFrom;
+        cout << "digraph G {" << endl;
+        for (int i = 0; i < size; i++) {
+            for (int j = i; j < size; j++) {
+                if (matrix[i][j] != 0) {
+                    string from = nodes[i]->name;
+                    string to = nodes[j]->name;
+                    if (find(printedTo.begin(), printedTo.end(), to) == printedTo.end() ||
+                        find(printedFrom.begin(), printedFrom.end(), from) == printedFrom.end()) {
+                        cout << "    " << from << " -> " << to << " [label=\"" << matrix[i][j] << "\", arrowhead=none];" << endl;
+                        printedTo.push_back(to);
+                        printedFrom.push_back(from);
+                    }
                 }
             }
-            return nullptr;
+        }
+        cout << "}" << endl;
+    }
+
+    int searchRoute(string name, string name2) {
+        /*
+         * Поиск кратчайшего пути между двумя вершинами
+         */
+
+        int index = getIndex(name); // Индекс начальной вершины
+        int index2 = getIndex(name2); // Индекс конечной вершины
+        if (index == -1 || index2 == -1) { // Если вершины не найдены
+            cout << "Error: node not found" << endl;
+            return -1;
         }
 
-        void print() {
-            /*
-             * Вывод графа на экран
-             */
+        // Массив, содержащий расстояния от начальной вершины до всех остальных
+        vector<int> distances(size, -1);
 
-            vector<string> printedTo;
-            vector<string> printedFrom;
-            cout << "digraph G {" << endl;
+        // Массив, содержащий индексы вершин, которые уже были просмотрены
+        vector<int> visited(size, 0);
+        distances[index] = 0;
+        visited[index] = 1;
+        int current = index; // Начинаем с начальной вершины
+
+        // Пока не просмотрены все вершины
+        while (current != index2) {
+
+            // Просматриваем все ребра, исходящие из текущей вершины
             for (int i = 0; i < size; i++) {
-                for (int j = 0; j < nodes[i]->neighbours.size(); j++) {
-                    bool isPrinted = false;
-                    for (int k = 0; k < printedTo.size(); k++) {
-                        if (printedFrom[k] == nodes[i]->name && printedTo[k] == nodes[i]->neighbours[j]->name) {
-                            isPrinted = true;
-                            break;
-                        }
-                    }
-                    if (!isPrinted) {
-                        cout << nodes[i]->name << " -> " << nodes[i]->neighbours[j]->name << " [label=\"" << nodes[i]->distances[j] << "\"];" << endl;
-                        printedTo.push_back(nodes[i]->name);
-                        printedFrom.push_back(nodes[i]->neighbours[j]->name);
 
+                // Если ребро существует и вершина еще не была просмотрена
+                if (matrix[current][i] != 0 && visited[i] == 0) {
+
+                    // Если расстояние до вершины еще не было вычислено, либо вычисленное расстояние больше, чем расстояние через текущую вершину
+                    if (distances[i] == -1 || distances[i] > distances[current] + matrix[current][i]) {
+
+                        // Обновляем расстояние до вершины
+                        distances[i] = distances[current] + matrix[current][i];
                     }
-                }
-                if (nodes[i]->neighbours.empty()) {
-                    cout << nodes[i]->name << endl;
                 }
             }
-            cout << "}" << endl;
+
+            // Ищем вершину с минимальным расстоянием
+            int min = -1;
+
+            // Просматриваем все вершины
+            for (int i = 0; i < size; i++) {
+
+                // Если вершина еще не была просмотрена и расстояние до нее было вычислено
+                if (visited[i] == 0 && distances[i] != -1) {
+
+                    // Если минимальное расстояние еще не было вычислено, либо вычисленное минимальное расстояние больше, чем расстояние до текущей вершины
+                    if (min == -1 || distances[i] < distances[min]) {
+                        min = i;
+                    }
+                }
+            }
+
+            // Если минимальное расстояние не было вычислено, значит путь не существует
+            if (min == -1) {
+                break;
+            }
+            current = min;
+
+            // Помечаем вершину как просмотренную
+            visited[current] = 1;
         }
-        
+
+        return distances[index2];
+    }
+
 };
 
 int main() {
@@ -132,8 +220,9 @@ int main() {
     graph.addEdge("S", "M", 15);
     graph.addEdge("D", "M", 21);
 
-
-
     graph.print();
+
+    cout << "Distance L - R: " << graph.searchRoute("L", "D") << endl;
+    cout << "Distance R - M: " << graph.searchRoute("R", "M") << endl;
     return 0;
 }
