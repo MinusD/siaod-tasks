6d115a0ec794f053d01d92027dd200753cfe2452
Дмитрий <50549999+MinusD@users.noreply.github.com>
2022-11-13 21:23:17 +0300

    Added Practice 6 (file)

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -327,16 +327,16 @@ int main() {
 //            21.3, 22.3, 23.3, 24.3, 25.3, 26.3, 27.3, 28.3, 29.3, 30.3,
 //            31.3, 32.3, 33.3, 34.3, 35.3, 36.3, 37.3, 38.3, 39.3, 40.3,
     };
-//    // Перемешать значения
-//    for (int i = 0; i < values.size(); i++) {
-//        int index = rand() % values.size();
-//        double temp = values[i];
-//        values[i] = values[index];
-//        values[index] = temp;
-//    }
-//    for (double value: values) {
-//        tree.add(value);
-//    }
+    // Перемешать значения
+    for (int i = 0; i < values.size(); i++) {
+        int index = rand() % values.size();
+        double temp = values[i];
+        values[i] = values[index];
+        values[index] = temp;
+    }
+    for (double value: values) {
+        tree.add(value);
+    }
 
     // Текстовое меню
     int choice = 0;
